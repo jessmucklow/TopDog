cb1d23941727301614ff08af66c1876b8c51ec9f
jess <jessicamucklow@gmail.com>
2022-07-26 09:44:35 -0700

    update

--- models/ranks.js
+++ models/ranks.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const reviewSchema = new Schema({
+const rankSchema = new Schema({
   rating: {
     type: Number,
     min: 1,
