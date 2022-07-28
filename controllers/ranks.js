92465f63fbb2226c9556dc7fc34a5ad3779c3c48
jess <jessicamucklow@gmail.com>
2022-07-28 11:44:53 -0700

    update

--- controllers/ranks.js
+++ controllers/ranks.js
@@ -2,8 +2,33 @@ const Pet = require('../models/pet');
 
 module.exports = {
     index,
-    create
+    create,
+    delete: deleteRank,
+    edit,
+    update
 };
+
+function edit(req, res) {
+    console.log(req.params.id)
+      Pet.findOne({"ranks._id": req.params.id}, function(err, pet) {
+       let rank = pet.ranks.id(req.params.id)
+       console.log(rank)
+        if (err || !pet) return res.redirect('/pets');
+            res.render('pets/edit', {pet, rank, title: 'edit'});
+      });
+    }
+  
+  function update(req, res) {
+    Pet.findOne({'ranks._id': req.params.id}, function(err, pet) {
+      const rankUpdate = pet.ranks.find(x => x.id === req.params.id);
+      if (!rankUpdate.user.equals(req.user._id)) return res.redirect(`pets/edit/${pet._id}`);
+      rankUpdate.ranking = req.body.ranking;
+      pet.save(function(err) {
+        res.redirect(`/pets/show/${pet._id}`);
+      });
+    });
+  }
+  
 
 function index(req, res) {
     Pet.find({}).sort('-createdAt').exec(function (err, pets) {
@@ -11,18 +36,17 @@ function index(req, res) {
     });
 }
 
-// function create(req, res) {
-//     Pet.findById(req.params.id, function(err, rank) {
-//       req.body.user = req.user._id;
-//       req.body.userName = req.user.name;
-//       req.body.userAvatar = req.user.avatar;
-//       pet.ranks.push(req.body);
-//       pet.save(function(err) {
-//         res.redirect(`/pets/${pet._id}`);
-//       });
-//     });
-//   }
-
+async function deleteRank(req, res, next) {
+    try {
+      const pet = await Pet.findOne({'ranks._id': req.params.id, 'ranks.user': req.user._id});
+      if (!pet) throw new Error('Nice Try!');
+      pet.ranks.remove(req.params.id);
+      await pet.save();
+      res.redirect(`/pets/show/${pet._id}`);
+    } catch (err) {
+      return next(err);
+    }
+  }
 
 function create(req, res) {
     Pet.findById(req.params.id, function (err, pet) {
--- routes/ranks.js
+++ routes/ranks.js
@@ -6,4 +6,8 @@ console.log('made it to router')
 
 router.get('/', ranksCtrl.index);
 router.post('/:id', ranksCtrl.create);
+router.delete('/:id', ranksCtrl.delete);
+router.get('/:id/edit', ranksCtrl.edit);
+router.put('/:id', ranksCtrl.update);
+
 module.exports = router;
